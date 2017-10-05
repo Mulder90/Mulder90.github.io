ace4665ebddd89abf6553356ceaa94022adda2b8
Lorenzo Cinque <lore.cinque@gmail.com>
2017-10-05 16:53:02 +0200

    Change SAMPLE_AD_TAG of videojs-ima with a Moat VPAID

--- videojs-ima/ads.js
+++ videojs-ima/ads.js
@@ -38,7 +38,7 @@ var Ads = function () {
   this.player.ima(this.options);
 }
 
-Ads.prototype.SAMPLE_AD_TAG = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
+Ads.prototype.SAMPLE_AD_TAG = 'https://svastx.moatads.com/dentsupgitalysizmekvideo27850520180/SWIFFER_-_Italy_-_ISP_SWIFFER_OTTOBRE_17_268182_2017_10-22605300_js.xml';
 
 Ads.prototype.init = function () {
   this.player.ima.initializeAdDisplayContainer();
